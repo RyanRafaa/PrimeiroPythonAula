572ee6a31df169872fb326a401fb4a962ae7024b
unknown <ryanrafael11@hotmail.com>
2023-02-27 22:23:22 -0300

    primeiro exrc

---
+++ Ryan.py
@@ -0,0 +1,41 @@
+# Primeira aula Python
+# print("1 + 1")
+
+# print(22,33,44)
+
+# print(22, 33, 44, sep="###")
+
+# print("Ryan", 32, sep="#-#-#-#")
+
+# print(30 == 30)
+
+# print(30 == 31)
+
+# print(type (30 == 31))
+
+# print(type ("1 + 1"))
+
+# print(type (22))
+
+# print(type (1.1))
+
+
+
+# nome = "Ryan Rafael"
+# idade = 2023 - 2001
+
+# print (nome)
+# print (str(idade)+" anos")
+# print (idade, "anos")
+
+nome = "Ryan"
+sobrenome = "Rafael"
+idade = 2023-2001
+altura = 1.73
+peso = 62
+
+print ("nome: " + nome + " "+ sobrenome + ", idade: "+ str(idade) +" anos, altura: "+ str(altura) +"metros, peso: "+ str(peso) +" Kg")
+if idade > 17:
+        print ("maior de idade")
+else:
+    ("menor de idade")
