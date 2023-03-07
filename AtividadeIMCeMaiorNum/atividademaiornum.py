d2f43277f0212470c30eb998cd90f37278504622
unknown <ryanrafael11@hotmail.com>
2023-03-06 22:00:45 -0300

    f

---
+++ AtividadeIMCeMaiorNum/atividademaiornum.py
@@ -0,0 +1,11 @@
+
+num1 = float (input("Digite um número: "))
+num2 = float (input("Digite outro número: "))
+
+
+if num1 == num2:
+    print ("Os número são iguais")
+elif num1 > num2:
+    print ("O maior número é ",num1)
+elif num2 > num1:
+    print ("O maior número é ",num2)
---
+++ Aula06-03/operador.py
@@ -0,0 +1,37 @@
+# adicao = 2+2
+# subtracao = 2-2
+# divisao = 18/3
+# multiplicacao = 2*2
+# divisao_inteiro= 10/3
+# modulo = 10 % 4
+
+# print(adicao)
+# print(subtracao)
+# print("A multiplicação é " , multiplicacao) 
+# print("A divisão é " , divisao) 
+# print("A divisão é " , divisao_inteiro) 
+# print("O módulo é " , modulo) 
+# vinte_vezes = "Univale - " *20
+
+# print(vinte_vezes)
+
+# a = 'Ryan'
+# b = 22
+# c = 'anos'
+
+# texto = 'O meu nome é: {nome}'
+# exibir = texto.format(nome=a)
+
+# print(exibir)
+# texto2= 'Tenho {0} {1}'
+
+# exibir = texto2.format(b,c)
+
+# print(exibir)
+
+# nome = input("Qual o seu nome? ")
+
+# time = input("Santista sofre mais que azeitona em boca de banguelo? ")
+
+pergunta = input("Qual sua cidade de nascimento? ")
+
