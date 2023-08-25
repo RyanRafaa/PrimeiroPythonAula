233010493546bf2a55b25f3f25e146fe5d0ffe1a
Ryan Rafael <112643867+RyanRafaa@users.noreply.github.com>
2023-08-24 21:40:59 -0300

    Add files via upload

---
+++ aulahj/aula.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+matriz = np.array ([[2,3,1], [4,5,7], [9,0,6]])
+
+
+
+for i in range (matriz.shape[0]):
+    print(matriz[i])
+
+for i in range (matriz.shape[0]):
+    print(matriz[i])
+    for d in range (matriz.shape[1]):
+        print(matriz[i][d])
