f7c4d81433b2a6304860f824bae36de954785250
Ryan Rafael <112643867+RyanRafaa@users.noreply.github.com>
2023-10-05 08:06:04 -0300

    Update pt2.py

--- aula28-09/pt2.py
+++ aula28-09/pt2.py
@@ -1,10 +1,34 @@
-from teste import Pessoa
+class Curso:
+    def __init__(self, nome, duracao):
+        self.nome = nome
+        self.duracao = duracao
 
-class Programador(Pessoa):
-    def __init__(self, nome, linguagem):
-        super().__init__(nome)
-        self.linguagem = linguagem
-        print(f"programador {self.nome}, linguagem: {self.linguagem}")
+    def detalhes_do_curso(self):
+        return f"Curso: {self.nome}, Duração: {self.duracao} anos"
 
 
-maria = Programador("Maria", "Python")
+class Presencial(Curso):
+    def __init__(self, nome, duracao, numero_de_vagas):
+        super().__init__(nome, duracao)
+        self.numero_de_vagas = numero_de_vagas
+
+    def detalhes_do_curso(self):
+        curso_base = super().detalhes_do_curso()
+        return f"{curso_base}, Vagas Presenciais: {self.numero_de_vagas}"
+
+
+class Online(Curso):
+    def __init__(self, nome, duracao, plataforma_online):
+        super().__init__(nome, duracao)
+        self.plataforma_online = plataforma_online
+
+    def detalhes_do_curso(self):
+        curso_base = super().detalhes_do_curso()
+        return f"{curso_base}, Plataforma Online: {self.plataforma_online}"
+
+
+curso_presencial = Presencial("Curso Presencial", 2, 30)
+curso_online = Online("Curso Online", 3, "Plataforma Univale")
+
+print(curso_presencial.detalhes_do_curso())
+print(curso_online.detalhes_do_curso())
